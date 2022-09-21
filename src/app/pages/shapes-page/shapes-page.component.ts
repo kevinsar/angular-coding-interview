ff7f881205e63e4005eabf4be9bff037bd0859e9
kevinsar <kevin@kevinsar.com>
2022-09-21 10:15:19 -0700

    Fix stackblitz setup

--- src/app/pages/shapes-page/shapes-page.component.ts
+++ src/app/pages/shapes-page/shapes-page.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { ServiceMockService } from 'src/app/service-mock.service';
+import { ServiceMockService } from '../..//service-mock.service';
 
 @Component({
   selector: 'app-shapes-page',
   templateUrl: './shapes-page.component.html',
-  styleUrls: ['./shapes-page.component.scss']
+  styleUrls: ['./shapes-page.component.scss'],
 })
 export class ShapesPageComponent implements OnInit {
   randomNumbers: number[] = [];
-  constructor(private serviceMockService: ServiceMockService) {
+  constructor(private serviceMockService: ServiceMockService) {}
 
-  }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   getRandomNumbers(): void {
-    this.serviceMockService.getMockServiceItem().subscribe((numbers: number[]) => {
-      this.randomNumbers = numbers;
-    }, error => {
-      console.log(error);
-      this.randomNumbers = [];
-    })
+    this.serviceMockService.getMockServiceItem().subscribe(
+      (numbers: number[]) => {
+        this.randomNumbers = numbers;
+      },
+      (error) => {
+        console.log(error);
+        this.randomNumbers = [];
+      }
+    );
   }
 }
